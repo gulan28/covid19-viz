3e39f48fcdbb5fadab6684cae7034f369c6881d2
gulan28 <62241776+gulan28@users.noreply.github.com>
2020-03-16 22:16:50 +0530

    Add data for 16th march + fix infected counts for 15th march

--- dataIndex.js
+++ dataIndex.js
@@ -2,5 +2,6 @@ var dataIndex = {
  '12-3-2020': {file: 'data_12_3_2020.csv', infected: 19},
  '13-3-2020': {file: 'data_13_3_2020.csv', infected: 22},
  '14-3-2020': {file: 'data_14_3_2020.csv', infected: 22},
- '15-3-2020': {file: 'data_15_3_2020.csv', infected: 25},
+ '15-3-2020': {file: 'data_15_3_2020.csv', infected: 24},
+ '16-3-2020': {file: 'data_16_3_2020.csv', infected: 27}
 }
