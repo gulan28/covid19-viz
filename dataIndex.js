47bdf3f39a03abd51119a86a05da843bc08c6778
gulan28 <62241776+gulan28@users.noreply.github.com>
2020-03-17 13:21:45 +0530

    Adding data for 9th, 10th and 11th + Removing infected cases count
    Removing infected cases count as the bulletins aren't exactly
    clear on the number of infected people.
    
    http://dhs.kerala.gov.in/wp-content/uploads/2020/03/Daily-Bulletin-HFWD-English-March-15th-1.pdf
    
    The table says 21 while the text says 22 etc.

--- dataIndex.js
+++ dataIndex.js
@@ -1,7 +1,10 @@
 var dataIndex = {
- '12-3-2020': {file: 'data_12_3_2020.csv', infected: 19},
- '13-3-2020': {file: 'data_13_3_2020.csv', infected: 22},
- '14-3-2020': {file: 'data_14_3_2020.csv', infected: 22},
- '15-3-2020': {file: 'data_15_3_2020.csv', infected: 24},
- '16-3-2020': {file: 'data_16_3_2020.csv', infected: 27}
+ '9-3-2020': {file: 'data_9_3_2020.csv'},
+ '10-3-2020': {file: 'data_10_3_2020.csv'},
+ '11-3-2020': {file: 'data_11_3_2020.csv'},
+ '12-3-2020': {file: 'data_12_3_2020.csv'},
+ '13-3-2020': {file: 'data_13_3_2020.csv'},
+ '14-3-2020': {file: 'data_14_3_2020.csv'},
+ '15-3-2020': {file: 'data_15_3_2020.csv'},
+ '16-3-2020': {file: 'data_16_3_2020.csv'},
 }
