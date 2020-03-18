4659481334dea8f6e4937b373d4c8a11c78e28d5
gulan28 <62241776+gulan28@users.noreply.github.com>
2020-03-19 01:19:56 +0530

    Multiple changes (listed)
      - Added python script to create pivot files (total.csv)
      - Show dates in reverse order
      - Changed the format in which data is entered in dataIndex.js

--- README.md
+++ README.md
@@ -3,12 +3,14 @@
 This is a visualisation of COVID-19 spread in Kerala courtesy [http://dhs.kerala.gov.in/](http://dhs.kerala.gov.in/)
 Cleaned data is in the `data` folder.
 
-### How you can help
+### Adding new daily bulletin data
 
-* Wash your hands :)
-* The data is currently manually entered from the PDF files hosted on DHS website. You can add missing day's data by taking a screenshot of the table in pdf and using [https://online2pdf.com/convert-jpg-to-excel#](https://online2pdf.com/convert-jpg-to-excel#) to convert it into an excel file and then exporting it as csv. The file might contain misspelled characters. Fix them and format them according to `/data/data_*.csv` csv's and send a PR
-* TODO items need to be fixed.
+* The data is currently manually entered from the PDF files hosted on DHS website. You can add missing day's data by taking a screenshot of the table in pdf and using [https://online2pdf.com/convert-jpg-to-excel#](https://online2pdf.com/convert-jpg-to-excel#) to convert it into an excel file and then exporting it as csv. The file might contain misspelled characters. Fix them and format them according to `/data/data_*.csv` csv's.
+* Once you have done this, `cd` to the parent project folder and run
+  `python pivot_maker.py`
+  Which will create a new `total.csv` in the data folder with the total values. You can commit that new file and issue a PR.
 
 ### Todo
+* Wash your hands :)
 * Show total values + infected counts (this is in dataIndex.js)
 * No leading zeroes in dates because of how dates are parsed
---
+++ pivot_maker.py
@@ -0,0 +1,61 @@
+import csv
+import glob
+import os
+import re
+
+FILE_REG = re.compile('^data_(?P<day>\d+)_(?P<month>\d+)_(?P<year>\d+)\.csv', re.I)
+
+def process_csv():
+    csvpath = './data/*.csv'
+    csvlist = glob.glob(csvpath)
+    data = {}
+    for filepath in csvlist:
+        filename = os.path.basename(filepath)
+        if FILE_REG.match(filename):
+            rows = []
+            with open(filepath, 'r') as csvfile:
+                for r in csv.DictReader(csvfile):
+                    # cleaning up data. remove empty keys
+                    if '' in r:
+                        del r['']
+                    rows.append(r)
+                data[filename] = rows
+    return data
+
+
+def get_date(filename):
+    match_obj = FILE_REG.search(filename)
+    if match_obj:
+        day = match_obj.group('day')
+        month = match_obj.group('month')
+        year = match_obj.group('year')
+        #  return in YYYY-MM-DD format for js Date.parse to work
+        return "{}-{}-{}".format(year, month, day)
+    return None
+
+
+def csv_writer(filepath, data):
+    with open(filepath, 'w') as f:
+        w = csv.DictWriter(f, fieldnames=set([key for row in data for key in row.keys()]))
+        w.writeheader()
+        w.writerows(data)
+
+
+def create_total_pivot():
+    total_list = []
+    print("Creating total pivot file")
+    data = process_csv()
+    for fname in data.keys():
+        date = get_date(fname)
+        for i in data[fname]:
+            if i['district'].lower() == 'total':
+                del i['district']
+                i['date'] = date
+                total_list.append(i)
+    csv_writer('./data/total.csv', total_list)
+    print("Created")
+
+
+if __name__ == '__main__':
+    create_total_pivot()
+                
