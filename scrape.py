5a577be235bf14259b980c18cfbb680b40c41db7
gulan28 <62241776+gulan28@users.noreply.github.com>
2020-04-24 21:38:39 +0530

    Add session to scraper to store coookies + cosmetic changes

--- scrape.py
+++ scrape.py
@@ -8,31 +8,34 @@ import sys
 
 
 district_map = {
-    "TVM": "Thiruvananthapuram",
-    "KLM": "Kollam",
-    "PTA": "Pathanamthitta",
-    "IDK": "Idukki",
-    "KTM": "Kottayam",
-    "ALP": "Alappuzha",
-    "EKM": "Ernakulam",
-    "TSR": "Thrissur",
-    "PKD": "Palakkad",
-    "MPM": "Malappuram",
-    "KKD": "Kozhikode",
-    "WND": "Wayanad",
-    "KNR": "Kannur",
-    "KGD": "Kasaragod",
-    "TOTAL": "Total"
+    'TVM': 'Thiruvananthapuram',
+    'KLM': 'Kollam',
+    'PTA': 'Pathanamthitta',
+    'IDK': 'Idukki',
+    'KTM': 'Kottayam',
+    'ALP': 'Alappuzha',
+    'EKM': 'Ernakulam',
+    'TSR': 'Thrissur',
+    'PKD': 'Palakkad',
+    'MPM': 'Malappuram',
+    'KKD': 'Kozhikode',
+    'WND': 'Wayanad',
+    'KNR': 'Kannur',
+    'KGD': 'Kasaragod',
+    'TOTAL': 'Total'
 }
 
 CSV_HEADER = ['district', 'observation', 'total_hospitalized', 'isolation', 'hospitalized_today']
-REQ_URL = "https://dashboard.kerala.gov.in/quarantine-view-public.php"
+INIT_URL = 'https://dashboard.kerala.gov.in'
+REQ_URL = 'https://dashboard.kerala.gov.in/quarantine-view-public.php'
 DATE_REQ_STRING = 'rep_date'
 HEADERS = {
     'Sec-Fetch-Dest': 'document',
     'Sec-Fetch-Site': 'same-origin',
     'Sec-Fetch-Mode': 'navigate',
     'Sec-Fetch-User': '?1',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
+    'Origin': 'https://dashboard.kerala.gov.in',
     'Referer': 'https://dashboard.kerala.gov.in/quarantine-view-public.php'
 }
 
@@ -45,15 +48,17 @@ def csv_writer(filepath, data):
 
 def get_data_for_date(date):
     # expects a date object
-    date_str = date.strftime("%d/%m/%Y")
+    date_str = date.strftime('%d/%m/%Y')
     payload = {DATE_REQ_STRING: date_str, 'vw': 'View'}
     csv_data = []
     try:
-        response = requests.post(REQ_URL, data=payload, headers=HEADERS)
+        sess = requests.Session()
+        _throwaway = sess.get(INIT_URL, headers=HEADERS)
+        response = sess.post(REQ_URL, data=payload, headers=HEADERS)
     except requests.exceptions.RequestException  as e:
         print(e)
     else:
-        soup = BeautifulSoup(response.content, "html.parser")
+        soup = BeautifulSoup(response.content, 'html.parser')
         table = soup.find_all('table')[0]
         for row in table.find_all('tr')[1:]:
             cols = [i.text for i in row.find_all('td')]
