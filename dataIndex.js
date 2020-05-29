4e01672e782e7ab9e77fc8b1bfab522157c29c9d
gulan28 <62241776+gulan28@users.noreply.github.com>
2020-05-29 21:03:19 +0530

    Added data for 29th May

--- dataIndex.js
+++ dataIndex.js
@@ -89,6 +89,7 @@ var dataIndex = {
     '26-5-2020': {file: 'data_26_5_2020.csv', sample_sent: 56704, sample_negative: 54836, total_active: 415, total_positive: 963, deaths: 6},
     '27-5-2020': {file: 'data_27_5_2020.csv', sample_sent: 58866, sample_negative: 56558, total_active: 445, total_positive: 1003, deaths: 6},
     '28-5-2020': {file: 'data_28_5_2020.csv', sample_sent: 60685, sample_negative: 58460, total_active: 526, total_positive: 1088, deaths: 7},
+    '29-5-2020': {file: 'data_29_5_2020.csv', sample_sent: 62746, sample_negative: 60448, total_active: 577, total_positive: 1150, deaths: 8},
   },
   pivot: {file: 'pivot.json'}
 }
