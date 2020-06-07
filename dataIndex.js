3f9fe17fd7a0fb8e9163779b3a0a8687f6f2c4ad
gulan28 <62241776+gulan28@users.noreply.github.com>
2020-06-07 21:06:55 +0530

    Added data for 7th June

--- dataIndex.js
+++ dataIndex.js
@@ -98,6 +98,7 @@ var dataIndex = {
     '4-6-2020': {file: 'data_4_6_2020.csv', sample_sent: 76383, sample_negative: 72139, total_active: 884, total_positive: 1588, deaths: 14, positive_today: 94},
     '5-6-2020': {file: 'data_5_6_2020.csv', sample_sent: 79074, sample_negative: 74769, total_active: 973, total_positive: 1699, deaths: 14, positive_today: 111},
     '6-6-2020': {file: 'data_6_6_2020.csv', sample_sent: 81517, sample_negative: 77517, total_active: 1029, total_positive: 1807, deaths: 15, positive_today: 108},
+    '7-6-2020': {file: 'data_7_6_2020.csv', sample_sent: 83875, sample_negative: 79957, total_active: 1095, total_positive: 1914, deaths: 15, positive_today: 107},
   },
   pivot: {file: 'pivot.json'}
 }
