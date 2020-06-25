ee7aff2f6fa94e92fd9091fb6db1743a2329f9b2
gulan28 <62241776+gulan28@users.noreply.github.com>
2020-06-25 21:58:58 +0530

    Added data for 25th June

--- dataIndex.js
+++ dataIndex.js
@@ -116,6 +116,7 @@ var dataIndex = {
     '22-6-2020': {file: 'data_22_6_2020.csv', total_passengers: 308180, sample_sent: 139686, sample_negative: 135880, total_active: 1540, total_positive: 3310, deaths: 21, positive_today: 138},
     '23-6-2020': {file: 'data_23_6_2020.csv', total_passengers: 315664, sample_sent: 144649, sample_negative: 139368, total_active: 1620, total_positive: 3451, deaths: 22, positive_today: 141},
     '24-6-2020': {file: 'data_24_6_2020.csv', total_passengers: 322967, sample_sent: 148827, sample_negative: 143131, total_active: 1691, total_positive: 3603, deaths: 22, positive_today: 152},
+    '25-6-2020': {file: 'data_25_6_2020.csv', total_passengers: 333050 , sample_sent: 156401, sample_negative: 150458, total_active: 1761, total_positive: 3726, deaths: 22, positive_today: 123},
   },
   pivot: {file: 'pivot.json'}
 }
