75232af35e405c68aa7d62d7afce20c7b04bf89e
gulan28 <62241776+gulan28@users.noreply.github.com>
2020-08-20 00:02:14 +0530

    Added data for 19th August

--- dataIndex.js
+++ dataIndex.js
@@ -172,6 +172,7 @@ var dataIndex = {
     '16-8-2020': {file: 'data_16_8_2020.csv', total_passengers: 734891, sample_sent: 511471, sample_negative: 487905, total_active: 15310, total_positive: 44415, deaths: 156, positive_today: 1530},
     '17-8-2020': {file: 'data_17_8_2020.csv', total_passengers: 739913, sample_sent: 516391, sample_negative: 494428, total_active: 15890, total_positive: 46140, deaths: 169, positive_today: 1725},
     '18-8-2020': {file: 'data_18_8_2020.csv', total_passengers: 746048, sample_sent: 528006, sample_negative: 502189, total_active: 16274, total_positive: 47898, deaths: 175, positive_today: 1758},
+    '19-8-2020': {file: 'data_19_8_2020.csv', total_passengers: 751661, sample_sent: 537887, sample_negative: 511196, total_active: 17382, total_positive: 50231, deaths: 182, positive_today: 2333},
   },
   pivot: {file: 'pivot.json'}
 }
