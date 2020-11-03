759d10f8069464f95d642324e313b9f5c5c48de1
gulan28 <62241776+gulan28@users.noreply.github.com>
2020-11-03 22:32:36 +0530

    Adding data for 2020-11-03

--- dataViz.js
+++ dataViz.js
@@ -68,7 +68,7 @@ Object.keys(dataIndex.daily_bulletin).forEach(function(key) {
   var tpRateToday = (item.positive_today > 0 ? item.positive_today / totTodayDoneSample * 100 : 0);
   tpRate.push(tpRateToday.toFixed(2));
   // subtracted 4. refer to https://dashboard.kerala.gov.in/dailyreporting.php
-  recovered.push((item.total_positive - (item.total_active + item.deaths + 108)));
+  recovered.push((item.total_positive - (item.total_active + item.deaths + 113)));
 });
 
 tpRateMovAvg = movingAvg(7, positiveToday, totSample);
